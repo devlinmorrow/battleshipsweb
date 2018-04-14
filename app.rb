ba944d0c0b643c2505e4256b4d575307d2292f12
Devlin Glasman <devlin.glasman@gmail.com>
2018-04-14 17:13:49 +0100

    Added second way of persisting data using hidden

--- app.rb
+++ app.rb
@@ -58,4 +58,55 @@ get "/game/:points" do
 end
 
 
+get "/game/version2" do
+  @grid_state = params[:grid_state]
+  @grid_state += "#{params[:point]}"
+  if @grid_state.include?("01") 
+    @point_01 = "O"
+  else 
+    @point_01 = "X"
+  end
+  if @grid_state.include?("02") 
+    @point_02 = "O"
+  else 
+    @point_02 = "X"
+  end
+  if @grid_state.include?("03") 
+    @point_03 = "O"
+  else 
+    @point_03 = "X"
+  end
+  if @grid_state.include?("04") 
+    @point_04 = "O"
+  else 
+    @point_04 = "X"
+  end
+  if @grid_state.include?("05") 
+    @point_05 = "H"
+  else 
+    @point_05 = "X"
+  end
+  if @grid_state.include?("06") 
+    @point_06 = "H"
+  else 
+    @point_06 = "X"
+  end
+  if @grid_state.include?("07") 
+    @point_07 = "O"
+  else 
+    @point_07 = "X"
+  end
+  if @grid_state.include?("08") 
+    @point_08 = "O"
+  else 
+    @point_08 = "X"
+  end
+  if @grid_state.include?("09") 
+    @point_09 = "O"
+  else 
+    @point_09 = "X"
+  end
+  @grid_points = [@point_01, @point_02, @point_03, "<br>", @point_04, @point_05, @point_06, "<br>", @point_07, @point_08, @point_09]
+  erb :version2
+end
 
