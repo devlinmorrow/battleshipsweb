7719596aa78ece87b60a66d3b4678df28d57a64f
Devlin Glasman <devlin.glasman@gmail.com>
2018-04-19 20:57:21 +0100

    Game won/lost added. Added messages.

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ enable :sessions
 
 get "/" do
   session[:guesses_left] = 10
+  session[:boats_left] = 5
   session[:game_grid] = Grid.new
   session[:boat_1] = Boat.new([2,3])
   session[:boat_2] = Boat.new([14,19])
@@ -30,7 +31,7 @@ get "/play_game" do
   if params[:user_input]
     user_input = params[:user_input].to_i
 
-  #conversion of user input to corresponding grid array element number
+    #conversion of user input to corresponding grid array element number
     if user_input < 6
       session[:array_number] = user_input - 1
     end
@@ -47,26 +48,36 @@ get "/play_game" do
       session[:array_number] = user_input + 3
     end
 
-  #check if grid point is hit or miss and run mechanics accordingly
-  if session[:boat_list].any_boat_hit?(user_input)
-    session[:game_grid].record_hit(session[:array_number])
-    boat_hit = session[:boat_list].which_boat_hit?(user_input)
-    boat_hit.record_hit(user_input)
-    if boat_hit.sunk?
-      session[:boat_sunk_message]
+    #check if grid point is hit or miss and run mechanics accordingly
+    if session[:boat_list].any_boat_hit?(user_input)
+      @hit_message = "You hit one!"
+      session[:game_grid].record_hit(session[:array_number])
+      boat_hit = session[:boat_list].which_boat_hit?(user_input)
+      boat_hit.record_hit(user_input)
+      if boat_hit.sunk?
+        @boat_sunk_message = "... and yay, you sank a boat!"
+      end
+    else
+      @miss_message = "Ah shucks, you missed so you lose a guess."
+      session[:game_grid].record_miss(session[:array_number])
+      session[:guesses_left] -= 1
     end
-  else
-    session[:game_grid].record_miss(session[:array_number])
-    session[:guesses_left] -= 1
+
+    #check if game won or lost
+    if session[:boat_list].all_boats_sunk?
+      redirect "/win"
+    elsif session[:guesses_left] == 0
+      redirect "/lose"
+    end
   end
-  end
-  erb :play_game
+  session[:boats_not_sunk] = session[:boat_list].count_boats_not_sunk
+    erb :play_game
 end
 
-get "/win" do
-  erb :win
-end
+  get "/win" do
+    erb :win
+  end
 
-get "/lose" do
-  erb :lose
-end
+  get "/lose" do
+    erb :lose
+  end
